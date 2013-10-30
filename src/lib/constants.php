f4e704724125b829454b85ce394a44a58615a2d5
Kevin Herrera <kevin@herrera.io>
2013-10-30 10:07:17 -0700

    Checking if already defined.

--- src/lib/constants.php
+++ src/lib/constants.php
@@ -1,8 +1,12 @@
 <?php
 
-/**
- * The manifest schema file.
- *
- * @var string
- */
-define('PHAR_UPDATE_MANIFEST_SCHEMA', __DIR__ . '/../../res/schema.json');
+if (!defined('PHAR_UPDATE_MANIFEST_SCHEMA')) {
+
+    /**
+     * The manifest schema file.
+     *
+     * @var string
+     */
+    define('PHAR_UPDATE_MANIFEST_SCHEMA', __DIR__ . '/../../res/schema.json');
+
+}
